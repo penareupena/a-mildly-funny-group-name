f78258a45b8c6b753330bcb36c757299e457e598
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 17:20:03 +1300

    added blank attributes class

---
+++ src/Attribute.java
@@ -0,0 +1,30 @@
+/**
+ * This class represents one of the attributes that a character may have
+ * <p>
+ * 		A character can have any number of attributes, as long as they have
+ * 		unique names.
+ * </p>
+ * <p>
+ * 		The possible attribute names are:
+ * 		<ul>
+ * 			<li>Level</li>
+ * 			<li>Experience</li>
+ * 			<li>Health</li>
+ * 			<li>Mana</li>
+ * 			<li>Physical Damage</li>
+ * 			<li>Magic Damage</li>
+ * 			<li>Armour</li>
+ * 			<li>Magical Resistance</li>
+ * 			<li>Critical Chance</li>
+ * 			<li>Strength</li>
+ * 			<li>Dexterity</li>
+ * 			<li>Power</li>
+ * 		</ul>
+ * </p>
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public class Attribute {
+
+}
