c92b52eff05dc8aa1cc3b0e121f06783571ff864
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 17:47:13 +1300

    added Item class

---
+++ src/Item.java
@@ -0,0 +1,14 @@
+/**
+ * This class represents an item that a player may collect throughout the game.
+ * 
+ * <p>
+ * 		These items are things like potions which can be used on characters
+ * 		that are associated with the player.
+ * </p>
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public class Item extends InventoryItem {
+
+}
