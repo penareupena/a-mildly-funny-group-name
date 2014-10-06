7703244ad65988d0d45deb6818ec1eb18bd29627
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 17:41:05 +1300

    added InventoryItem class

---
+++ src/InventoryItem.java
@@ -0,0 +1,9 @@
+/**
+ * This abstract class represents a generic item that could be in the player's inventory.
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public abstract class InventoryItem {
+
+}
