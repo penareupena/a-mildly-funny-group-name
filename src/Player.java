bcaf890c660410e112c01ce4dc9e3869b5299364
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 16:49:43 +1300

    added blank player class with docs

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+/**
+ * This class represents the player in the game.
+ * <p>
+ * It has holds all the data about the inventory, party and characters
+ * that are associated with the player, as well as the data about where
+ * it is placed in the overworld.
+ * </p>
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public class Player {
+	
+}
