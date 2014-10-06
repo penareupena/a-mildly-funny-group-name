148e0f16fb92bac75aabc32c9c525ec581e4eb72
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 17:49:53 +1300

    added Accessory class

---
+++ src/Accessory.java
@@ -0,0 +1,15 @@
+/**
+ * This class represents an accessory that the player may equip to any character
+ * that is associated with him.
+ * 
+ * <p>
+ * 		Each accessory has a type, and only a certain number of accessories of the
+ * 		same type may be equipped to a single player.
+ * </p>
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public class Accessory extends InventoryItem {
+
+}
