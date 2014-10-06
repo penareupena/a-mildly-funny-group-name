c14a29ab307af93a87ef5d57a8df1a4e63bae797
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 17:43:36 +1300

    added Skillbook class

---
+++ src/Skillbook.java
@@ -0,0 +1,14 @@
+/**
+ * This class represents a skillbook item that the player can collect through out the game.
+ * 
+ * <p>
+ * 		A skillbook item is used to learn skills. Each skill requires a number of skillbook
+ * 		items to be able to be learnt.
+ * </p>
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public class Skillbook {
+
+}
