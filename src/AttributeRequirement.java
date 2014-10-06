a2ad764b2cb6aa1962f54003d68ef81204bfa7a5
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 17:39:01 +1300

    added AttributeRequirement class

---
+++ src/AttributeRequirement.java
@@ -0,0 +1,14 @@
+/**
+ * This class represents an attribute requirement for a skill to be learned.
+ * 
+ * <p>
+ * 		A skill can have any number of attribute requirements provided that they are
+ * 		for different attributes.
+ * </p>
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public class AttributeRequirement {
+
+}
