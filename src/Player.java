d475c51576451128c7afce99007a5794afc91c82
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 18:57:31 +1300

    created Player constructor with attribute values, insatiates characters, party and inventory

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * This class represents the player in the game.
  * <p>
@@ -9,6 +11,25 @@
  * @author A Mildly Funny Group Name
  *
  */
+
 public class Player {
+	
+	private ArrayList<Character> characters;	// the characters that are associated with the player
+	private Party party;						// the party of characters to be used in battle
+	private Inventory inventory;				// the inventory of items that the player has obtained
+	
+	/**
+	 * Creates an instance of player to be used through out the game
+	 * 
+	 * @param strength the number of strength points the initial character will have
+	 * @param dexterity the number of dexterity points the initial character will have
+	 * @param power the number of power points the initial character will have
+	 */
+	public Player(int strength, int dexterity, int power) {
+		characters = new ArrayList<Character>();
+		party = new Party();
+		inventory = new Inventory();
+	}
+	
 	
 }
