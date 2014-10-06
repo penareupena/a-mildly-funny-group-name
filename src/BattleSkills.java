4d0ef12c41d14d24b22b7f70560d66f96e2d15b7
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-06 17:32:58 +1300

    added BattleSkills class

---
+++ src/BattleSkills.java
@@ -0,0 +1,14 @@
+/**
+ * This class represents the skills that a character has been equipped for battle
+ * 
+ * <p>
+ * 		Any number of skills can be added to the battle skills, up to the max 
+ * 		specified in the constructor.
+ * </p>
+ * 
+ * @author A Mildly Funny Group Name
+ *
+ */
+public class BattleSkills {
+
+}
