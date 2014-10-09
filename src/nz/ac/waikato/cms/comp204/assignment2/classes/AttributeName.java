f579fe9985c899f769cad4b055a5302c2f0c2e81
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-09 22:37:25 +1300

    added AttributeName.java

---
+++ src/nz/ac/waikato/cms/comp204/assignment2/classes/AttributeName.java
@@ -0,0 +1,16 @@
+package nz.ac.waikato.cms.comp204.assignment2.classes;
+
+public enum AttributeName {
+	level,
+	xp,
+	hp,
+	mana,
+	physicalDamage,
+	magicDamage,
+	armour,
+	magical,
+	critical,
+	strength,
+	dexterity,
+	power
+}
