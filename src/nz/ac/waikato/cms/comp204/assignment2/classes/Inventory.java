7b303d2d703ddfcc02e03959766371a3974726f9
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-12 21:44:16 +1300

    Added add method to Inventory.java

--- src/nz/ac/waikato/cms/comp204/assignment2/classes/Inventory.java
+++ src/nz/ac/waikato/cms/comp204/assignment2/classes/Inventory.java
@@ -23,12 +23,24 @@ public class Inventory {
 	}
 	
 	/**
-	 * Adds 
+	 * Adds a given item to the inventory
+	 * <p>
+	 * 		If the item of the same type (ie. name) has already been added
+	 * 		to the inventory, the quantity is incremented by the quantity
+	 * 		of the given item, otherwise the object is added to the inventory.
+	 * </p>
 	 *  
 	 * @param item the item to add to the inventory
 	 * 
 	 */
 	public void add(InventoryItem item) {
+		for (int i = 0; i < items.size(); i++) {
+			if (items.get(i).ofType() == item.ofType() ) {
+				items.get(i).changeQuantity(item.getQuantity());
+				return;
+			}
+		}
 		
+		items.add(item);
 	}
 }
