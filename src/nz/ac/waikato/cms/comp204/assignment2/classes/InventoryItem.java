7914e4250353de3c61ad88505e2ed041352f5df6
Sean Coon <sc292@students.waikato.ac.nz>
2014-10-12 21:25:53 +1300

    Added constructors and ofType method to InventoryItem.java

--- src/nz/ac/waikato/cms/comp204/assignment2/classes/InventoryItem.java
+++ src/nz/ac/waikato/cms/comp204/assignment2/classes/InventoryItem.java
@@ -5,6 +5,38 @@ package nz.ac.waikato.cms.comp204.assignment2.classes;
  * @author A Mildly Funny Group Name
  *
  */
-public abstract class InventoryItem {
-
+public class InventoryItem {
+	protected int quantity;
+	protected String name;
+	
+	/**
+	 * Creates a new instance of InventoryItem.
+	 * 
+	 * @param name the name of the item
+	 * @param quantity the quantity of the item
+	 */
+	public InventoryItem(String name, int quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
+	/**
+	 * Create a new instance of InventoryItem
+	 * <p>
+	 * 		This creates an InventoryItem which has a quantity of 1
+	 * </p>
+	 * 
+	 * @param name the name of the item
+	 */
+	public InventoryItem(String name) { 
+		this(name, 1);
+	}
+	
+	/**
+	 * Returns the string representation of the type of item it is, ie. the name
+	 * @return
+	 */
+	public String ofType() {
+		 return name;
+	}
 }
